ee96b24028944bb49dac603e0df7a7e0e3dbb64d
amba86 <>
2021-10-31 20:00:16 +0100

    implement bargraph

--- qbargraph.cpp
+++ qbargraph.cpp
@@ -1,6 +1,161 @@
 #include "qbargraph.h"
 
-QBarGraph::QBarGraph()
-{
+#include <QStylePainter>
 
+QBarGraph::QBarGraph(QWidget *parent,
+                     QColor backgroundColor,
+                     QColor cursorColor,
+                     QColor textColor,
+                     QColor progressColor,
+                     QBrush progressBrush,
+                     int cornerRadius,
+                     int cursorOffset,
+                     int padding) : QProgressBar(parent) {
+    this->backgroundColor = backgroundColor;
+    this->cursorColor = cursorColor;
+    this->textColor = textColor;
+    this->progressColor = progressColor;
+    this->progressBrush = progressBrush;
+    this->cornerRadius = cornerRadius;
+    this->cursorOffset = cursorOffset;
+    this->padding = padding;
+}
+
+QBarGraph::~QBarGraph() {
+    // NOP
+}
+
+void QBarGraph::paintEvent(QPaintEvent *event) {
+    Q_UNUSED(event);
+
+    // TODO: make member
+    QPainter painter(this);
+    painter.setRenderHints(QPainter::Antialiasing |
+                           QPainter::TextAntialiasing);
+
+    paintBackground(&painter);
+    paintProgress(&painter);
+    paintCursor(&painter);
+}
+
+void QBarGraph::paintBackground(QPainter *painter) {
+    painter->save();
+    painter->setPen(Qt::NoPen);
+    painter->setBrush(backgroundColor);
+
+    QRect rect(padding, 0, width() - padding * 2, height() / 3);
+    painter->drawRoundedRect(rect, cornerRadius, cornerRadius);
+
+    painter->restore();
+}
+
+void QBarGraph::paintProgress(QPainter *painter) {
+    painter->save();
+    painter->setPen(Qt::NoPen);
+    painter->setBrush(progressBrush == Qt::NoBrush ? progressColor : progressBrush);
+
+    double step = value() / (double)(maximum() - minimum());
+    int progress = (width() - padding * 2) * step;
+
+    QRect rect(padding, 0, progress, height() / 3);
+    painter->drawRoundedRect(rect, cornerRadius, cornerRadius);
+
+    painter->restore();
+}
+
+void QBarGraph::paintCursor(QPainter *painter) {
+    painter->save();
+    painter->setPen(Qt::NoPen);
+    painter->setBrush(progressBrush == Qt::NoBrush ? cursorColor : progressBrush);
+
+    double step = value() / (double)(maximum() - minimum());
+    int progress = (width() - padding * 2) * step;
+
+    QRect rect(progress, height() / 3 + cursorOffset, padding * 2, height() / 3);
+    painter->drawRoundedRect(rect, 4, 4);
+
+    // Paint cursor
+    int centerX = rect.center().x();
+    int y = height() / 3;
+    QPolygon polygon;
+    polygon.append(QPoint(centerX, y));
+    polygon.append(QPoint(centerX + cursorOffset, y + cursorOffset));
+    polygon.append(QPoint(centerX - cursorOffset, y + cursorOffset));
+    painter->drawPolygon(polygon);
+
+    // Paint text
+    double percent = value() / (double)(maximum() - minimum()) * 100;
+    QString text = QString("%1%").arg(percent, 0, 'f', 0);
+    painter->setPen(textColor);
+    // TODO: set font
+    //    painter->setFont();
+    painter->drawText(rect, Qt::AlignCenter, text);
+
+    painter->restore();
+}
+
+QColor QBarGraph::getBackgroundColor() const {
+    return backgroundColor;
+}
+
+void QBarGraph::setBackgroundColor(const QColor &value) {
+    backgroundColor = value;
+}
+
+QColor QBarGraph::getCursorColor() const {
+    return cursorColor;
+}
+
+void QBarGraph::setCursorColor(const QColor &value) {
+    cursorColor = value;
+}
+
+QColor QBarGraph::getTextColor() const {
+    return textColor;
+}
+
+void QBarGraph::setTextColor(const QColor &value) {
+    textColor = value;
+}
+
+QColor QBarGraph::getProgressColor() const {
+    return progressColor;
+}
+
+void QBarGraph::setProgressColor(const QColor &value) {
+    progressColor = value;
+}
+
+int QBarGraph::getCursorOffset() const
+{
+    return cursorOffset;
+}
+
+void QBarGraph::setCursorOffset(int value)
+{
+    cursorOffset = value;
+}
+
+QBrush QBarGraph::getProgressBrush() const {
+    return progressBrush;
+}
+
+void QBarGraph::setProgressBrush(const QBrush &value) {
+    progressBrush = value;
+}
+
+int QBarGraph::getCornerRadius() const {
+    return cornerRadius;
+}
+
+void QBarGraph::setCornerRadius(int value) {
+    cornerRadius = value;
+}
+
+int QBarGraph::getPadding() const {
+    return padding;
+}
+
+void QBarGraph::setPadding(int value) {
+    padding = value;
 }
--- qbargraph.h
+++ qbargraph.h
@@ -1,11 +1,65 @@
 #ifndef QBARGRAPH_H
 #define QBARGRAPH_H
 
+#include <QProgressBar>
 
-class QBarGraph : public QProgressBar
-{
+class QBarGraph : public QProgressBar {
+    Q_OBJECT
+
 public:
-    QBarGraph();
+    QBarGraph(QWidget *parent = nullptr,
+              QColor backgroundColor = Qt::gray,
+              QColor cursorColor = Qt::yellow,
+              QColor textColor = Qt::black,
+              QColor progressColor = Qt::yellow,
+              QBrush progressBrush = Qt::NoBrush,
+              int cornerRadius = 0,
+              int cursorOffset = 5,
+              int padding = 0);
+
+    ~QBarGraph() Q_DECL_OVERRIDE;
+
+    QColor getBackgroundColor() const;
+    void setBackgroundColor(const QColor &value);
+
+    QColor getCursorColor() const;
+    void setCursorColor(const QColor &value);
+
+    QColor getTextColor() const;
+    void setTextColor(const QColor &value);
+
+    QColor getProgressColor() const;
+    void setProgressColor(const QColor &value);
+
+    QBrush getProgressBrush() const;
+    void setProgressBrush(const QBrush &value);
+
+    int getCornerRadius() const;
+    void setCornerRadius(int value);
+
+    int getCursorOffset() const;
+    void setCursorOffset(int value);
+
+    int getPadding() const;
+    void setPadding(int value);
+
+protected:
+    void paintEvent(QPaintEvent *event) Q_DECL_OVERRIDE;
+    void paintBackground(QPainter *painter);
+    void paintCursor(QPainter *painter);
+    void paintProgress(QPainter *painter);
+
+private:
+    QColor backgroundColor;
+    QColor cursorColor;
+    QColor textColor;
+    QColor progressColor;
+
+    QBrush progressBrush;
+
+    int cornerRadius;
+    int cursorOffset;
+    int padding;
 };
 
 #endif // QBARGRAPH_H
