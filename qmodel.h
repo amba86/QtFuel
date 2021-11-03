a96535f8274d20e7c956efc988c24f9d92c32aa3
amba86 <>
2021-11-03 17:12:32 +0100

    insert doc comments

--- qmodel.h
+++ qmodel.h
@@ -3,25 +3,70 @@
 
 #include <QObject>
 
+/*!
+ * \brief The QModel class that receives external data and prepares it for viewing.
+ */
 class QModel : public QObject {
     Q_OBJECT
 
 public:
+
+    /*!
+     * \brief QModel Creates new model instance.
+     * \param parent If parent is 0, the new widget becomes a window. If parent is another widget, this widget becomes a child window inside parent.
+     */
     explicit QModel(QObject *parent = nullptr);
+
+    /*!
+     * Destroys the model instance.
+     */
     ~QModel() Q_DECL_OVERRIDE;
 
+    /*!
+     * \brief getFuelLevel Returns the fuel level.
+     * \return The fuel level.
+     */
     int getFuelLevel() const;
 
 public slots:
+
+    /*!
+     * \brief setSlope Sets the slope.
+     * \param newValue An integer value is converted to a boolean value.
+     */
     void setSlope(int newValue);
+
+    /*!
+     * @brief setFuelLevel Sets the fuel level. The fuel level is set when
+     * the range between the current value and the given value is greater than a threshold.
+     * Emits the fuelLevelChanged(int) signal when the fuel level changes.
+     * @param newValue The fuel level.
+     */
     void setFuelLevel(int newValue);
 
 signals:
+
+    /*!
+     * \brief fuelLevelChanged Emits the fuelLevelChanged(int) signal when the fuel level changes.
+     * \param newFuelLevel The current fuel level.
+     */
     void fuelLevelChanged(int newFuelLevel);
 
 private:
+
+    /*!
+     * \brief slope Indicates slope state.
+     */
     bool slope;
+
+    /*!
+     * \brief fuelLevel The current fuel level.
+     */
     int fuelLevel;
+
+    /*!
+     * \brief threshold The fuel level is updated only when it is above the threshold of 5% (10% for slope).
+     */
     double threshold;
 
 };
--- qsensor.h
+++ qsensor.h
@@ -3,21 +3,50 @@
 
 #include <QThread>
 
+/*!
+ * \brief The QSensor class simulates a physical fuel level sensor and generates data in the range -255 to 255.
+ */
 class QSensor : public QThread {
     Q_OBJECT
 
 public:
+
+    /*!
+     * \brief QSensor Creates new sensor instance.
+     * \param parent If parent is 0, the new widget becomes a window. If parent is another widget, this widget becomes a child window inside parent.
+     */
     explicit QSensor(QObject *parent = nullptr);
+
+    /*!
+     * Destroys the instance.
+     */
     ~QSensor() Q_DECL_OVERRIDE;
 
 signals:
+
+    /*!
+     * \brief valueChanged Emits the valueChanged(int) signal when new data is created.
+     * \param newValue The data representing the fuel level.
+     */
     void valueChanged(int newValue);
 
 protected:
+
+    /*!
+     * \brief run The starting point for the thread. New data is generated using the cosine wave.
+     */
     void run();
 
 private:
+
+    /*!
+     * \brief value The data representing the fuel level.
+     */
     int value;
+
+    /*!
+     * \brief angle The current cosine angle.
+     */
     double angle;
 
 };
